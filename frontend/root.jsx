0afaa83ae8f01291998726767d33a614863fb824
Kelly <kjk224@cornell.edu>
2020-01-26 16:35:51 -0800

    first commit

---
+++ frontend/root.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+class Root extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                React is not broken
+            </div>
+        )
+    }
+}
+
+export default Root;
